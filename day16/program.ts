3346c76eafab66f1ce8fee4d3d1d62f00bde8d9f
colindembovsky <colindembovsky@gmail.com>
2023-12-01 10:40:04 -0600

    Clean startup

--- day16/program.ts
+++ day16/program.ts
@@ -2,7 +2,7 @@ import { loadInput, dayName, Difficulty } from "../utils/readUtils";
 
 let day = dayName(__dirname);
 let contents = loadInput(__dirname, Difficulty.EASY);
-//let contents = loadInput(__dirname, false);
+
 console.log(contents);
 
 console.log(`==== ${day}: PART 1 ====`);
