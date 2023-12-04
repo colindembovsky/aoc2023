2e2855e9783f7f374cadb7fc701517969adea7fd
colindembovsky <colindembovsky@gmail.com>
2023-12-04 10:51:45 -0600

    try part 2

--- day04/card.ts
+++ day04/card.ts
@@ -1,11 +1,14 @@
 export class Card {
+    static originalCards: Card[] = [];
+    static matchMap: Map<number, number> = new Map<number, number>();
+
     id: number = 0;
     nums: number[] = [];
     winNums: number[] = [];
     matches = 0;
     points = 0;
     constructor(line: string) {
-        this.id = parseInt(line.split(":")[0].split(" ")[1]);
+        this.id = parseInt(line.split(":")[0].match(/\d+/)![0]);
         this.nums = line.split(":")[1].split("|")[0].trim().split(" ").filter(x => x.trim() !== "").map(x => parseInt(x));
         this.winNums = line.split(":")[1].split("|")[1].trim().split(" ").filter(x => x.trim() !== "").map(x => parseInt(x));
         this.matches = this.nums.filter(x => this.winNums.includes(x)).length;
@@ -13,12 +16,19 @@ export class Card {
             case 0:
                 this.points = 0;
                 break;
-            case 1:
-                this.points = 1;
-                break;
             default:
                 this.points = Math.pow(2, this.matches - 1);
                 break;
+        }
+    }
+
+    calculateRecursiveWinners() {
+        if ((this.matches === 0)) return;
+
+        for (let i = this.id; i < Math.min(this.id + this.matches, Card.originalCards.length - 1); i++) {
+            let wCard = Card.originalCards[i];
+            Card.matchMap.set(wCard.id, Card.matchMap.get(wCard.id)! + 1);
+            wCard.calculateRecursiveWinners();
         }
     }
 }
--- day04/program.ts
+++ day04/program.ts
@@ -10,3 +10,17 @@ let cards = lines.map(x => new Card(x));
 let totalPoints = cards.reduce((acc, card) => acc + card.points, 0);
 
 console.log(totalPoints);
+
+console.log(`==== ${day}: PART 2 ====`);
+Card.originalCards = cards;
+// initialize the map with the card IDs
+cards.forEach(x => Card.matchMap.set(x.id, 1));
+cards.forEach(x => x.calculateRecursiveWinners());
+// sum the values of the map
+// print the key:value pairs of the map
+console.log(Array.from(Card.matchMap.entries()).map(x => `${x[0]}: ${x[1]}`).join("\n"));
+let totalRecursivePoints = Array.from(Card.matchMap.values()).reduce((acc, val) => acc + val, 0);
+console.log(totalRecursivePoints);
+// 633585 is too low
+// 633559 is too low
+// 5278062 is too low
