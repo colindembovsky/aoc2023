e0153a3f161d11436e1f41bff6e44a4f9c93c675
colindembovsky <colindembovsky@gmail.com>
2023-12-05 11:54:27 -0600

    cleanup

--- day05/program.ts
+++ day05/program.ts
@@ -4,10 +4,7 @@ import { Almanac } from "./map";
 let day = dayName(__dirname);
 let contents = loadInput(__dirname, Difficulty.HARD);
 
-console.log(contents);
-
 console.log(`==== ${day}: PART 1 ====`);
 let almanac = new Almanac(contents);
 let locations = almanac.seeds.map(x => almanac.getLocationForSeed(x));
-console.log(locations);
 console.log(Math.min(...locations));
