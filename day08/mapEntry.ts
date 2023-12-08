8daea51a73e746d2f23a517be2d1c16b80061c81
colindembovsky <colindembovsky@gmail.com>
2023-12-08 11:14:22 -0600

    try brute force

--- day08/mapEntry.ts
+++ day08/mapEntry.ts
@@ -4,25 +4,37 @@ interface MapEntry {
 }
 
 export class NodeMap {
-    nMap: Map<string, MapEntry> = new Map<string, MapEntry>();
-
-    constructor(public instructions: string, public lines: string[]) {
+    static nMap: Map<string, MapEntry> = new Map<string, MapEntry>();
+    static instructions: string;
+    static curStep = 0;
+    static parseMap(lines: string[]) {
         lines.forEach(line => {
             let [key, value] = line.split(" = ");
             let [left, right] = value.substring(1, value.length - 1).split(",").map(s => s.trim());
-            this.nMap.set(key, { left, right });
+            NodeMap.nMap.set(key, { left, right });
         });
     }
 
-    countStepsInInstructions(start = "AAA", target = "ZZZ"): number {
+    curPos = "";
+    constructor(public start: string) {
+        this.curPos = start;
+    }
+
+    countStepsInInstructions(target = "ZZZ"): number {
         let steps = 0;
-        let cur = start;
+        let cur = this.start;
         while (true) {
             if (cur === target) break;
-            let direction = this.instructions[steps++ % this.instructions.length];
-            let entry = this.nMap.get(cur)!;
+            let direction = NodeMap.instructions[steps++ % NodeMap.instructions.length];
+            let entry = NodeMap.nMap.get(cur)!;
             cur = direction === "L" ? entry.left : entry.right;
         }
         return steps;
+    }
+
+    next() {
+        let direction = NodeMap.instructions[NodeMap.curStep % NodeMap.instructions.length];
+        let entry = NodeMap.nMap.get(this.curPos)!;
+        this.curPos = direction === "L" ? entry.left : entry.right;
     }
 }
--- day08/program.ts
+++ day08/program.ts
@@ -10,11 +10,36 @@ let contents = loadInput(__dirname, Difficulty.HARD);
 // BBB = (AAA, ZZZ)
 // ZZZ = (ZZZ, ZZZ)`;
 
+// let contents3 = `LR
+
+// 11A = (11B, XXX)
+// 11B = (XXX, 11Z)
+// 11Z = (11B, XXX)
+// 22A = (22B, XXX)
+// 22B = (22C, 22C)
+// 22C = (22Z, 22Z)
+// 22Z = (22B, 22B)
+// XXX = (XXX, XXX)`;
+
 console.log(`==== ${day}: PART 1 ====`);
 let lines = contents.split("\n");
 let instructions = lines.shift()!;
 lines.shift(); // empty line
-let map = new NodeMap(instructions, lines);
-console.log(map.countStepsInInstructions());
+NodeMap.instructions = instructions;
+NodeMap.parseMap(lines);
 
+let map = new NodeMap("AAA");
+//console.log(map.countStepsInInstructions());
 
+console.log(`==== ${day}: PART 2 ====`);
+// find all nodes that end with "A"
+let nodes = [...NodeMap.nMap.keys()].filter(k => k.endsWith("A"));
+let maps = nodes.map(n => new NodeMap(n));
+
+while (true) {
+    maps.forEach(m => m.next());
+    NodeMap.curStep++;
+    if (maps.every(m => m.curPos.endsWith("Z"))) break;
+    if (NodeMap.curStep % 1000000 === 0) console.log(NodeMap.curStep);
+}
+console.log(NodeMap.curStep);
