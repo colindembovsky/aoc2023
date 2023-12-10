63bc1482582a9c36d21a5e4a162458eef70501ef
colindembovsky <colindembovsky@gmail.com>
2023-12-10 16:39:31 -0600

    trying

--- day05/program.ts
+++ day05/program.ts
@@ -2,20 +2,24 @@ import { loadInput, dayName, Difficulty } from "../utils/readUtils";
 import { Almanac } from "./map";
 
 let day = dayName(__dirname);
-let contents = loadInput(__dirname, Difficulty.HARD);
+let contents = loadInput(__dirname, Difficulty.EASY);
 
 console.log(`==== ${day}: PART 1 ====`);
 let almanac = new Almanac(contents);
-let locations = almanac.seeds.map(x => almanac.mapNum(x));
-console.log(Math.min(...locations));
+// let locations = almanac.seeds.map(x => almanac.mapNum(x));
+// console.log(Math.min(...locations));
 
 console.log(`==== ${day}: PART 2 ====`);
 let seedRange = almanac.seedRanges[0];
-let min = Number.MAX_SAFE_INTEGER;
-for (let i = seedRange.Start; i < seedRange.Start + seedRange.Length; i++) {
-    let location = almanac.mapNum(i, "seed", "location");
-    if (location < min) {
-        min = location;
+
+// create a range from seedRange.Start to seedRange.Start + seedRange.Length using the spread operator
+let range = [...Array(seedRange.Length).keys()].map(x => x + seedRange.Start);
+
+let minLocation = range.reduce((prev, cur) => {
+    let curLocation = almanac.mapNum(cur, "seed", "location");
+    if (curLocation < prev) {
+        return curLocation;
     }
-}
-console.log(min);
+    return prev;
+}, Number.MAX_SAFE_INTEGER);
+console.log(minLocation);
