1ee460729d66488646c9423a2244f09235b82240
colindembovsky <colindembovsky@gmail.com>
2023-12-11 15:01:09 -0600

    floodfill won't work

--- day10/program.ts
+++ day10/program.ts
@@ -1,7 +1,15 @@
 import { loadInput, dayName, Difficulty } from "../utils/readUtils";
 
 let day = dayName(__dirname);
-let contents = loadInput(__dirname, Difficulty.HARD);
+let contents = loadInput(__dirname, Difficulty.EASY);
+
+enum Type {
+    Junk,
+    Inside,
+    Wall,
+}
+
+let areaTracker : Type[][] = [];
 
 let test1 = `-L|F7
 7S-7|
@@ -110,15 +118,19 @@ function moveToNewPosAndDirection() {
         }
     }
     curLetter = getLetter(curRow, curCol);
+    areaTracker[curRow][curCol] = Type.Wall;
     console.log(`${curLetter} > ${Direction[facing]}`);
 }
 
 console.log(`==== ${day}: PART 1 ====`);
+// initialize the area track setting every cell to Junk
+areaTracker = Array.from({length: lines.length}, () => Array.from({length: lines[0].length}, () => Type.Junk));
 let [curRow, curCol] = findStart();
-//let curLetter = "F"; // easy
-let curLetter = "-"; // hard
-let facing = Direction.West;
+areaTracker[curRow][curCol] = Type.Wall;
 
+let curLetter = "F"; // easy
+//let curLetter = "-"; // hard
+let facing = Direction.West;
 let count = 1;
 moveToNewPosAndDirection();
 while(curLetter !== "S") {
@@ -126,3 +138,46 @@ while(curLetter !== "S") {
     count++;
 }
 console.log(count/2);
+
+console.log(`==== ${day}: PART 2 ====`);
+
+function floodFill(x: number, y: number) {
+    if (x < 0 || y < 0 || x >= areaTracker.length || y >= areaTracker[0].length) {
+        return;
+    }
+    if (areaTracker[x][y] !== Type.Junk) {
+        return;
+    }
+
+    areaTracker[x][y] = Type.Inside;
+
+    floodFill(x - 1, y);
+    floodFill(x + 1, y);
+    floodFill(x, y - 1);
+    floodFill(x, y + 1);
+}
+
+// Call floodFill from all 4 corners
+if (areaTracker[0][0] !== Type.Wall) {
+    floodFill(0, 0);
+}
+if (areaTracker[0][areaTracker[0].length - 1] !== Type.Wall) {
+    floodFill(0, areaTracker[0].length - 1);
+}
+// if (areaTracker[areaTracker.length - 1][0] !== Type.Wall) {
+//     floodFill(areaTracker.length - 1, 0);
+// }
+// if (areaTracker[areaTracker.length - 1][areaTracker[0].length - 1] !== Type.Wall) {
+//     floodFill(areaTracker.length - 1, areaTracker[0].length - 1);
+// }
+
+// count the number of cells that are inside
+let insideCount = 0;
+for (let i = 0; i < areaTracker.length; i++) {
+    for (let j = 0; j < areaTracker[0].length; j++) {
+        if (areaTracker[i][j] === Type.Inside) {
+            insideCount++;
+        }
+    }
+}
+console.log(insideCount);
