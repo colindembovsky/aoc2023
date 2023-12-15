c345977ebc8b74ef4f05439299159108a13a3cd1
colindembovsky <colindembovsky@gmail.com>
2023-12-15 10:45:03 -0600

    Solve part 1

--- day15/program.ts
+++ day15/program.ts
@@ -1,9 +1,18 @@
 import { loadInput, dayName, Difficulty } from "../utils/readUtils";
 
 let day = dayName(__dirname);
-let contents = loadInput(__dirname, Difficulty.EASY);
+let contents = loadInput(__dirname, Difficulty.HARD);
 
-console.log(contents);
+let segments = contents.split(",");
+
+function hash(input: string) {
+    return input.split("").reduce((acc, char) => {
+        let ascii = char.charCodeAt(0);
+        return (acc + ascii) * 17 % 256;
+    }, 0);
+}
 
 console.log(`==== ${day}: PART 1 ====`);
-
+// sum the hash of each segment
+let sum = segments.reduce((acc, segment) => acc + hash(segment), 0);
+console.log(sum);
