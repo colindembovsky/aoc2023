5fc413d61407c49417cdddfb81207e9878e7f553
colindembovsky <colindembovsky@gmail.com>
2023-12-17 12:11:16 -0600

    setup p1

--- day17/program.ts
+++ day17/program.ts
@@ -3,7 +3,5 @@ import { loadInput, dayName, Difficulty } from "../utils/readUtils";
 let day = dayName(__dirname);
 let contents = loadInput(__dirname, Difficulty.EASY);
 
-console.log(contents);
-
 console.log(`==== ${day}: PART 1 ====`);
 
