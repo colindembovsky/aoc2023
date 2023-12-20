c25ff1cc4e9301c902dc26e3214accd4c0ffbbeb
colindembovsky <colindembovsky@gmail.com>
2023-12-20 12:34:15 -0600

    solve p2

--- day17/pathfinder.ts
+++ day17/pathfinder.ts
@@ -40,10 +40,11 @@ export class PathFinder {
     visited = new Set<string>();
     heatLoss = 0;
     finished = false;
-    queue: Route[][] = [[{ position: new Position(0, 0), direction: Direction.right, distance: 0 }]];
+    queue: Route[][] = [];
 
-    constructor(lines: string[]) {
+    constructor(lines: string[], start: Route, private ultra = false) {
         this.grid = lines.map(l => l.split('').map(c => parseInt(c)));
+        this.queue.push([start]);
     }
 
     move(route: Route, direction: Direction) {
@@ -52,8 +53,11 @@ export class PathFinder {
             direction: direction,
             distance: direction === route.direction ? route.distance + 1 : 1
         };
-        if (next.distance > 3 || next.position.row < 0 || next.position.col < 0 || next.position.row >= this.grid.length || next.position.col >= this.grid[0].length) {
+        if (next.position.row < 0 || next.position.col < 0 || next.position.row >= this.grid.length || next.position.col >= this.grid[0].length) {
             return;
+        }
+        if (!this.ultra) {
+            if (next.distance > 3) return;
         }
 
         let key = `${next.position.row}-${next.position.col}-${next.direction.row}-${next.direction.col}-${next.distance}`;
@@ -73,9 +77,19 @@ export class PathFinder {
                 this.finished = true;
                 return this.heatLoss;
             }
-            this.move(route, route.direction);
-            this.move(route, route.direction.turnLeft());
-            this.move(route, route.direction.turnRight());
+            if (this.ultra) {
+                if (route.distance < 10) {
+                    this.move(route, route.direction);
+                }
+                if (route.distance >= 4) {
+                    this.move(route, route.direction.turnLeft());
+                    this.move(route, route.direction.turnRight());
+                }
+            } else {
+                this.move(route, route.direction);
+                this.move(route, route.direction.turnLeft());
+                this.move(route, route.direction.turnRight());
+            }
         }
         this.heatLoss++;
         return this.findRoute();
